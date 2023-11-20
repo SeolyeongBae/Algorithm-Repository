f41998b49500da2d8130a27946306ecb52d972eb
seolyeongbae <peixueying@gmail.com>
2023-11-20 21:54:59 +0900

    update BOJ 2839

--- BOJ_solution/2839.설탕 배달.py
+++ BOJ_solution/2839.설탕 배달.py
@@ -25,3 +25,32 @@ def sol(s):
 
 sugar = int(input())
 print(sol(sugar))
+
+
+# same solution 2023.ver -> 예전에 푼게 더 나아 보인다!!
+# 근데 더 직관적으로 하면 3의 몫을 빼고 나머지를 5로 나눌수 있는가 보는 거인 듯
+'''
+N = int(input())
+
+# 봉투는 3키로, 5키로가 있음
+# 최적화 해야 하는 부분 -> 봉투의 수
+
+dp = [-1, -1, -1, 1, -1, 1] #1부터 5까지, 1부터 인덱싱
+
+# dp[i] = i키로그램을 담는 경우 필요한 최소 봉투의 수
+# N >= 3
+
+if N == 4 or N == 5:
+    print(dp[N])
+
+else:
+    for i in range(6, N+1):
+        if max(dp[i-3], dp[i-5]) == -1:
+            dp.append(-1)
+        else:
+            if(dp[i-3] * dp[i-5] < 0):
+                dp.append(max(dp[i-3], dp[i-5]) + 1 )
+            else:
+                dp.append(min(dp[i-3], dp[i-5]) + 1)
+    print(dp)
+'''
